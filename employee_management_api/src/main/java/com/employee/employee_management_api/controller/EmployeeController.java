7753050929b386ebc0ff91444707fe72237321b2
Alamin <alamin8740@gmail.com>
2022-08-08 23:32:10 +0600

    Added Controller Class and Successfully create this api

---
+++ employee_management_api/src/main/java/com/employee/employee_management_api/controller/EmployeeController.java
@@ -0,0 +1,52 @@
+package com.employee.employee_management_api.controller;
+
+import com.employee.employee_management_api.model.Employee;
+import com.employee.employee_management_api.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    //    @GetMapping("/asdf")
+    //    public List<Employee> get(){
+    //        return employeeService.findAllEmployee();
+    //    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+        List<Employee> employees = employeeService.findAllEmployee();
+        return new ResponseEntity<>(employees,HttpStatus.OK);
+    }
+
+    @GetMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee,HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
+        Employee updateValue = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(updateValue,HttpStatus.OK);
+    }
+
+    @GetMapping("/delete/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id){
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
