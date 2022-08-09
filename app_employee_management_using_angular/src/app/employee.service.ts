3b1a61ac5f0bd9b10a18d2d4671d0549b643518b
Alamin <alamin8740@gmail.com>
2022-08-09 12:20:39 +0600

    Added employee.service.ts code inside angular

--- app_employee_management_using_angular/src/app/employee.service.ts
+++ app_employee_management_using_angular/src/app/employee.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../environments/environment";
+import {Observable} from "rxjs";
+import {Employee} from "./employee";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  // @ts-ignore
+  public getEmployees(): Observable<Employee[]>{
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+  }
+
+ public addEmployee(employee: Employee): Observable<Employee>{
+    return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`,employee);
+ }
+
+ public updateEmployee(employee: Employee): Observable<Employee>{
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`,employee);
+ }
+
+ // @ts-ignore
+  public deleteEmployee(employeeId: number): Observable<void>{
+    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+ }
+
 }
