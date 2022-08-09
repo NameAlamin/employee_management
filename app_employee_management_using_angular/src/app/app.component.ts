ea389108059a7528988c1b8a8c838a8aec220c7d
Alamin <alamin8740@gmail.com>
2022-08-09 14:59:07 +0600

    Added app.component.ts|.html and Successfully fetch all employee name form API

--- app_employee_management_using_angular/src/app/app.component.ts
+++ app_employee_management_using_angular/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EmployeeService} from "./employee.service";
+import {Employee} from "./employee";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app_employee_management_using_angular';
+export class AppComponent implements OnInit{
+  constructor(private employeeService: EmployeeService){} // inject employeeService Class
+
+  public employees: Employee[] | undefined;
+
+  public getEmployees(): void{
+    this.employeeService.getEmployees().subscribe(
+      (response) => {this.employees = response},
+      (error: HttpErrorResponse) => {alert(error.message)}
+    );
+  }
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+
+
 }
