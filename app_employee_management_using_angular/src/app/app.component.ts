c6087cc0c930367b8bdd1c1774ec730e680688b0
Alamin <alamin8740@gmail.com>
2022-08-09 20:30:33 +0600

    Configure modal button action now successfully show Modal

--- app_employee_management_using_angular/src/app/app.component.ts
+++ app_employee_management_using_angular/src/app/app.component.ts
@@ -8,7 +8,10 @@ import {HttpErrorResponse} from "@angular/common/http";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent implements OnInit{
+
   constructor(private employeeService: EmployeeService){} // inject employeeService Class
 
   public employees: Employee[] | undefined;
@@ -22,6 +25,31 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
     this.getEmployees();
+  }
+
+
+  // this method use for action add,edit and delete button
+
+  public onOpenModal(employee: Employee|null, mode: string): void{
+    const container = document.getElementById('main_container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle','modal');
+
+    if(mode === 'add'){
+      button.setAttribute('data-target','#addEmployeeModal');
+    }
+    if(mode === 'edit'){
+      button.setAttribute('data-target','#updateEmployeeModal');
+    }
+    if(mode === 'delete'){
+      button.setAttribute('data-target','#deleteEmployeeModal');
+    }
+    // @ts-ignore
+    container.appendChild(button);
+    button.click();
+
   }
 
 
